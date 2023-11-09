cd05ff0196ec441b6a5b51d46bdcacba291f0895
jakecolacoo <Jakecrane123111@gmail.com>
2023-11-08 20:31:48 -0700

    Started implementation of UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,26 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QMenu * menu = menuBar()->addMenu("File");
+
+    menu->addAction("Save");
+    menu->addAction("Save As");
+    menu->addAction("Load");
+
+    menu=menuBar()->addMenu("Presets");
+    menu->addAction("Insert Preset 1");
+    menu->addAction("Insert Preset 2");
+    menu->addAction("Insert Preset 3");
+    menu->addAction("Insert Preset 4");
+    menu->addAction("Insert Preset 5");
+
+    menu=menuBar()->addMenu("Theme");
+    menu->addAction("Dark Mode");
+    menu->addAction("Light Mode");
+    menu->addAction("Light Blue");
+    menu->addAction("Mocha");
+    menu->addAction("Rojo");
+
 }
 
 MainWindow::~MainWindow()
